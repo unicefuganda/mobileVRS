fd9bc39aabd90a45ba3c6faacb26c058bb67456f
Mugisha Moses <mossplix@gmail.com>
2012-06-25 16:52:45 +0300

    fix typo

--- mobilevrs_project/mobilevrs/urls.py
+++ mobilevrs_project/mobilevrs/urls.py
@@ -8,7 +8,7 @@ from mobilevrs.views import *
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'$/', ussd_menu),
+    url(r'/$', ussd_menu),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
